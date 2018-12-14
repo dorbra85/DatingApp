8d6437b6ea653cd857887133b9ed0786e4c41c0d
NoaDorDor <dorbra85@gmail.com>
2018-12-14 22:37:51 +0200

    hg

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 
 
 @Component({
-  // asd
   selector: 'app-value',
   templateUrl: './value.component.html',
   styleUrls: ['./value.component.css']
